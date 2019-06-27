14a6d7c649480bce88f93fce36e7fd8491d88633
duanxinxin <duanxinxins@163.com>
2019-06-27 21:07:27 +0800

    1.两数之和完成,需优化

---
+++ src/two-sum2/twoSum2.js
@@ -0,0 +1,12 @@
+var twoSum = function(numbers, target) {
+    let source = {};
+    let length = numbers.length;
+    let i = 0;
+    for(;i<length;i++){
+      let element = numbers[i]; 
+      if(source[target - element] !== void 0){
+
+      }
+    }
+};
+module.exports = twoSum;
